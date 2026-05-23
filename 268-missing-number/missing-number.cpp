34aac4f6da698c6861124c5dbc6ba3880eede30a
AMRENDRA-108 <kumaramrendra1512023@gmail.com>
2026-05-23 22:43:44 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (37.48%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size(); // if size = 5
+        int totalSum = n *(n+1)/2; // 15
+        // array ke saare element ka sum 
+        int actualSum = 0;
+        for(int num:nums){// nums ke saare value pe jayega ek ek krke
+        // if 4 missing
+            actualSum = actualSum + num; // add all current value of num (0 + 11) = 11
+
+        }
+        return totalSum - actualSum; //15- 11= 4
+    }
+};
