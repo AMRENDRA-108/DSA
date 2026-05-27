015c194571a93476a8610f8177bd29c95101069e
AMRENDRA-108 <kumaramrendra1512023@gmail.com>
2026-05-28 00:00:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (53.80%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int reversed_num = 0;
+        
+        while (x != 0) {
+            // 1. Pop the last digit
+            int pop = x % 10;
+            x /= 10;
+            
+            // 2. Check for overflow before multiplying by 10
+            // INT_MAX is 2147483647
+            if (reversed_num > INT_MAX / 10 || (reversed_num == INT_MAX / 10 && pop > 7)) {
+                return 0;
+            }
+            // INT_MIN is -2147483648
+            if (reversed_num < INT_MIN / 10 || (reversed_num == INT_MIN / 10 && pop < -8)) {
+                return 0;
+            }
+            
+            // 3. Push the digit
+            reversed_num = (reversed_num * 10) + pop;
+        }
+        
+        return reversed_num;
+    }
+};
