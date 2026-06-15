a04b8243af28b1bee7b71086e5aef7c3b85727a8
AMRENDRA-108 <kumaramrendra1512023@gmail.com>
2026-06-15 22:36:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (91.83%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -4,19 +4,19 @@ public:
         int n = haystack.length();
         int m = needle.length();
         
-        // Agar needle khali hai toh 0 return karein
+        // Agar needle khali hai toh 0 
         if (m == 0) return 0;
         
-        // Haystack par loop chalao jahan tak needle fit ho sake
+    
         for (int i = 0; i <= n - m; i++) {
             int j = 0;
             
-            // Match check karo
+            // Match check 
             while (j < m && haystack[i + j] == needle[j]) {
                 j++;
             }
             
-            // Agar poori needle match ho gayi toh starting index return karo
+            // Agar poori needle match ho gayi toh starting index return krange
             if (j == m) {
                 return i;
             }
