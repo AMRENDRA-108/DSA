83a172590d2e5ab9a14496e0be70233bcd6efc75
Amrendra Kumar <kumaramrendra1512023@gmail.com>
2026-07-01 22:38:12 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (48.31%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int k = 0; // This pointer tracks the position for the next valid element
-        
-        for (int i = 0; i < nums.size(); i++) { 
-            // If the current element is NOT the one we want to remove
-            if (nums[i] != val) { 
-                nums[k] = nums[i]; // Move it to the front
-                k++;               // Increment our valid element count
+        int k = 0;
+
+        for(int i = 0; i < nums.size() ; i++){
+            if(nums[i] != val){
+
+                nums[k] = nums[i];
+                k++;
+
             }
+            // Agar nums[i] == val hai, toh hum kuch nahi karenge.
+            // for loop ka 'i++' apne aap i ko agle element par le jayega.
         }
+        return k;
         
-        return k; // k is the number of elements not equal to val
     }
 };
